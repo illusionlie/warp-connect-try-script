8a7e3ef7ffc4d80c77ef299d9b65d3eafb832555
IllusionLie <skyflameendtimes@gmail.com>
2024-10-07 23:40:21 +0800

    chore: fix version number

--- GUI-for-WARP-Connect.bat
+++ GUI-for-WARP-Connect.bat
@@ -1,7 +1,7 @@
-:: GUI-for-WARP-Connect-Script v1.1.2-20241007
+:: GUI-for-WARP-Connect-Script v1.1.3-20241007
 :top
 endlocal
-set "warpcs-ver=v1.1.2"
+set "warpcs-ver=v1.1.3"
 set "warpcs-date=20241007"
 set "warpcs-title= -GUI-for-WARP-Connect-Script- %warpcs-ver%-%warpcs-date%"
 @echo off&title %warpcs-title%&cd /D "%~dp0"&color 70&setlocal enabledelayedexpansion&cls&chcp 936&mode con cols=80 lines=24
